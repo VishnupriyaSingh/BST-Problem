0d69a6405af34b33751422bd74212a8c2e6c5623
Vishnupriya <priya.singh9349@gmail.com>
2023-11-29 23:15:06 +0530

    Added code for Searching element in Binary Tree

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,23 +1,27 @@
-public class Main {
+public class MainUC3 {
     public static void main(String[] args) {
         BinaryTree<Integer> tree = new BinaryTree<>();
 
         tree.root = new TreeNode<>(56);
         tree.root.left = new TreeNode<>(30);
         tree.root.right = new TreeNode<>(70);
-        
+
         tree.root.left.left = new TreeNode<>(22);
         tree.root.left.right = new TreeNode<>(40);
         tree.root.left.left.left = new TreeNode<>(11);
-        
+
         tree.root.right.left = new TreeNode<>(60);
         tree.root.right.right = new TreeNode<>(95);
         tree.root.right.left.left = new TreeNode<>(65);
         tree.root.right.left.right = new TreeNode<>(63);
         tree.root.right.right.right = new TreeNode<>(67);
-        
+
         tree.printLevelOrder();
 
         System.out.println("\nThe size of the binary tree is: " + tree.size());
+
+        Integer searchValue = 63;
+        boolean found = tree.search(searchValue);
+        System.out.println("Is value " + searchValue + " present in the tree? " + found);
     }
 }
